0ab4fe907d9b51ecfc0e3ff79ab319b4272c7b1e
Lyarc79 <141921696+Lyarc79@users.noreply.github.com>
2026-06-10 15:08:08 +0200

    Changed port

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.set("view engine", "ejs");
 app.use("/", indexRouter);
 app.use(express.urlencoded({ extended: true }));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running at htpp://localhost:${PORT}`);
